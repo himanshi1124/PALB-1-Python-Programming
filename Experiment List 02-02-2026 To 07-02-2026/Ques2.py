6cc6825fdbc743ddd593735cca55e38da4316069
Himanshi Kumar <kumarhimanshi2004@gmail.com>
2026-04-06 14:44:08 +0530

    Ques2.py

--- Experiment List 02-02-2026 To 07-02-2026/Ques2.py
+++ Experiment List 02-02-2026 To 07-02-2026/Ques2.py
@@ -3,24 +3,24 @@
 class Solution:
     def smallestSubWithSum(self, x, arr):
         n = len(arr)
-        # Initialize current sum and minimum length
+       
         curr_sum = 0
         min_len = n + 1
         
-        # Initialize starting index
+       
         left = 0
         
-        # Iterate over the array
+       
         for right in range(n):
-            # Keep adding array elements while current sum is <= x
+            
             curr_sum += arr[right]
             
-            # Once current sum becomes greater than x
+            
             while curr_sum > x:
-                # Update minimum length if this window is smaller
+                
                 min_len = min(min_len, right - left + 1)
                 
-                # Shrink the window from the left to find a smaller subarray
+               
                 curr_sum -= arr[left]
                 left += 1
                 
